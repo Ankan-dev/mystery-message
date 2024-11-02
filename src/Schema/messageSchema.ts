4d433e910999f35429782938ac59fb6e372ef961
Ankan Mandal <ankanmandal2001@gmail.com>
2024-11-02 13:20:33 +0530

    added export in messageSchema

--- src/Schema/messageSchema.ts
+++ src/Schema/messageSchema.ts
@@ -1,6 +1,6 @@
 import {z} from "zod"
 
-const messageSchema=z.object({
+export const messageSchema=z.object({
     content:z.string()
     .min(10,{message:"The message should have atleast 10 characters"})
     .max(300,{message:"The message cannot have more than 300 characters"})
